ca6e9a0eef32bb58f48d658f897efd75766bf464
Stuart Blavatnik <stuartb@twoforboth.com>
2018-10-06 19:01:32 -0400

    Add files via upload

---
+++ main/java/Connected.java
@@ -0,0 +1,16 @@
+public class Connected {
+
+    public static void main(String[] args) {
+
+        Process process = new Process();
+
+        if (args.length != 3) {
+            System.out.println("Usage: Process filename city city");
+        }
+        else {
+            process.process(args[0], args[1], args[2]);
+            System.out.println(process.getStatus());
+        }
+
+    }
+}
---
+++ test/java/ProcessTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+public class ProcessTest {
+
+    private Process process;
+    private File file;
+
+    @Before
+    public void Prepare() {
+        process = new Process();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        file = new File(classLoader.getResource("cities.txt").getFile());
+
+    }
+
+
+    @Test
+    public void TestProcessFileNotFound() {
+
+        process.process("invalidFile.txt", "New York", "London");
+        assertEquals("File: invalidFile.txt not found", process.getStatus());
+    }
+
+    @Test
+    public void TestProcessTrueCaseBostonHartford() {
+        process.process(file.getAbsolutePath(), "Boston", "Hartford");
+        assertEquals("yes", process.getStatus());
+    }
+
+    @Test
+    public void TestProcessFalseCaseBostonTampa() {
+        process.process(file.getAbsolutePath(), "Boston", "Tampa");
+        assertEquals("no", process.getStatus());
+    }
+
+    @Test
+    public void TestProcessFalseCaseBostonYpsilanti() {
+        process.process(file.getAbsolutePath(), "Boston", "Ypsilanti");
+        assertEquals("no", process.getStatus());
+    }
+}
